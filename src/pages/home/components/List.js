b1c082b50ed91abfe73d6ba7b43648367b808f56
yun <344190651@qq.com>
2018-12-16 14:02:57 +0800

    nothing

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -3,36 +3,36 @@ import { ListWrapper, MoreList } from '../style' ;
 import { connect } from 'react-redux';
 import { action } from '../store';
 class List extends Component {
-    render() {
-        const { list, page, getMoreList } = this.props;
-        return (
-            <ListWrapper>
-                {
-                    list.map((item) => (
-                        <div className='list-item' key={item.get('id')}>
-                            <img 
-                                src={item.get('imgUrl')}
-                                className='list-pic'
-                                alt=''/>
-                            <div className='list-info'>
-                                <h3 className='article-title'>{item.get('title')}</h3>
-                                <p className='article-content'>{item.get('desc')}</p>
-                            </div>
-                        </div>
-                    ))
-                }
-                <MoreList onClick={ () => getMoreList(page)}>阅读更多</MoreList>
-            </ListWrapper>
-        )
-    }
+  render() {
+    const { list, page, getMoreList } = this.props;
+    return (
+      <ListWrapper>
+      {
+        list.map((item) => (
+          <div className='list-item' key={item.get('id')}>
+          <img 
+          src={item.get('imgUrl')}
+          className='list-pic'
+          alt=''/>
+          <div className='list-info'>
+          <h3 className='article-title'>{item.get('title')}</h3>
+          <p className='article-content'>{item.get('desc')}</p>
+          </div>
+          </div>
+        ))
+      }
+      <MoreList onClick={ () => getMoreList(page)}>阅读更多</MoreList>
+      </ListWrapper>
+      )
+  }
 }
 const mapState = (state) => ({
-    list: state.getIn(['home', 'articleList']),
-    page: state.getIn(['home', 'articlePage'])
+  list: state.getIn(['home', 'articleList']),
+  page: state.getIn(['home', 'articlePage'])
 });
 const mapDispatch = (dispatch) => ({
-    getMoreList(page) {
-        dispatch(action.getMoreList(page));
-    }
+  getMoreList(page) {
+    dispatch(action.getMoreList(page));
+  }
 })
 export default connect(mapState, mapDispatch)(List);
